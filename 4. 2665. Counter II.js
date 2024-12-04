e8c7a5214a9cd56f1c9116f4a65ef91eb2ee2ad9
Aditya Nandan <cse.adityanandan@gmail.com>
2024-12-05 02:51:08 +0530

    Ques. 04

---
+++ 4. 2665. Counter II.js
@@ -0,0 +1,15 @@
+var createCounter = function(init) {
+    let currentValue = init
+
+    return{
+        increment:function() {
+            return currentValue += 1
+        },
+        decrement:function() {
+            return currentValue -= 1
+        },
+        reset:function() {
+            return currentValue = init
+        }
+    }
+};
